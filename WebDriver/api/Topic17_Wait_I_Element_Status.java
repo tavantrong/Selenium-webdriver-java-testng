b697c30603821aa7f2cd353bfc1994213dfd2d4f
Trong Ta <tavantrong87@gmail.com>
2022-12-15 22:22:37 +0700

    Update

--- WebDriver/api/Topic17_Wait_I_Element_Status.java
+++ WebDriver/api/Topic17_Wait_I_Element_Status.java
@@ -1,13 +1,10 @@
 package api;
-import java.util.concurrent.TimeUnit;
 import org.testng.annotations.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
